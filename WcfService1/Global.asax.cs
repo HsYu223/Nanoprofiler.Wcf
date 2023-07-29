21befbdabd182e35aad1963e238a88fc2a53c09d
HsYu <hsiangyu223@hotmail.com>
2023-07-29 23:47:32 +0800

    加入專案檔案。

---
+++ WcfService1/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using EF.Diagnostics.Profiling;
+using EF.Diagnostics.Profiling.ProfilingFilters;
+using EF.Diagnostics.Profiling.Web.ProfilingFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WcfService1
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // register profiling filters to exclude some URLs from profiling
+            ProfilingSession.ProfilingFilters.Add(new NameContainsProfilingFilter("_tools/"));
+            ProfilingSession.ProfilingFilters.Add(new FileExtensionProfilingFilter("jpg", "js", "css"));
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
